b10e62454a72beb58ec22d3887ad23b37a68de30
Sean Pollock <seanpollock3344@gmail.com>
2019-07-05 17:09:51 -0400

    adding cors setup

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require("express")
-
+const axios = require("axios")
+const cors = require("cors")
 class HttpProxy {
   constructor() {
     this.app = express()
+    this.app.use(cors())
 
     this.app.get("/", (req, res) => {
       console.log("server web app bundle here")
+      res.send("transaction here")
     })
 
-    this.app.get("/contract", (req, res) => {
+    this.app.get("/contract", async (req, res) => {
       console.log("fetch from contract here")
+      const supply = await axios({
+        method: "GET",
+        url: "http://localhost:3002/supply",
+        data: JSON.stringify({}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      res.send(supply)
     })
 
-    this.app.listen(3001)
-    console.log("http server listening on port 3001")
+    this.app.listen(3002)
+    console.log("http server listening on port 3002")
   }
 }
 
